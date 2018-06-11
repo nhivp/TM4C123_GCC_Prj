23eab8489cf3a535a6ea68b341d653acfa282ca1
nhivp <41302791@hcmut.edu.vn>
2018-06-12 00:06:37 +0700

    Add a simple program

---
+++ wiki/prepare_environment/hello.c
@@ -0,0 +1,14 @@
+/*
+ * file: hello.c
+ * description: This is a simple program for testing the compiling environment
+ * author: Nhi Pham @ 2018
+ *
+ */
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    return 0;
+}
